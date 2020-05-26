3dd4b8c8c771f99083b060b565ccf83a46321351
wjosephmark <wjosephmark@gmail.com>
2020-05-26 16:35:39 -0600

    converted to hookrouter

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
+import { useRoutes, A } from "hookrouter";
 import App from "./components/app";
-import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import Auth from "./pages/auth"
 
 import "./style/main.scss";
 
-function main() {
-  ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+
+function Main() {
+  const [loggedInStatus, setLoggedInStatus] = useState("Not Logged In")
+  
+  const routes = () => {
+    if(loggedInStatus === "Logged In"){
+      return({
+        "/": () => <App />,
+        "/auth": () => <Auth />
+      })
+    } else {
+      return({
+        "/": () => <App />,
+        "/auth": () => <Auth loggedInStatus={loggedInStatus} setLoggedInStatus={setLoggedInStatus}/>
+      })
+    }
+  }
+
+  const handleSignOut = () => {
+    setLoggedInStatus("Not Logged In")
+  }
+
+  if(loggedInStatus === "Logged In") {
+    return(
+      <div>
+        <div className="nav-wrapper">
+          <div className="nav-link-wrapper">
+            <A href="/">Home</A>
+          </div>
+          <div className="nav-link-wrapper">
+            <A href="/auth">Employee Login</A>
+          </div>
+          <div>
+            <button className="btn" onClick={() => handleSignOut()}>Sign Out</button>
+          </div>
+        </div>
+        {useRoutes(routes())}
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        <div className="nav-wrapper">
+          <div className="nav-link-wrapper">
+            <A href="/">Home</A>
+          </div>
+          <div className="nav-link-wrapper">
+            <A href="/auth">Employee Login</A>
+          </div>
+        </div>
+        {useRoutes(routes())}
+      </div>
+    )
+  }
 }
 
-document.addEventListener("DOMContentLoaded", main);
+ReactDOM.render(<Main />, document.querySelector(".app-wrapper"))
---
+++ src/pages/auth.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from "react"
+
+export default function Auth() {
+    return(
+        <h1>Hello from auth.</h1>
+    )
+}
